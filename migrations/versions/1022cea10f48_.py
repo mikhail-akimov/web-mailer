3bbe4ae8580cc6938637e5e782b8441dcd2baf1c
makimov <daggerito@gmail.com>
2018-12-20 16:18:35 +0300

    Adds requirements

---
+++ migrations/versions/1022cea10f48_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1022cea10f48
+Revises: f431c403ce50
+Create Date: 2018-12-20 16:16:54.459170
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1022cea10f48'
+down_revision = 'f431c403ce50'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
